ff03b0995610eb4dd323e5c042e2a78cfb376c90
PaulaChristiana <paula.ciocu@emeal.nttdata.com>
2024-01-12 16:20:56 +0200

    Moved rest of the test to one class

--- test_1_project1.py
+++ test_1_project1.py
@@ -3,6 +3,11 @@ from project1 import count
 from project2 import average_run_time_excluding_system
 from project3 import count_app_failures3
 from project4 import count_app_failures
+from project5 import app_with_most_successful_runs
+from project6 import third_of_day_with_most_failures
+from project7 import run_times_per_app
+from project8 import busiest_hour_per_app
+from project9 import calculate_failure_rates
 
 class Test1(unittest.TestCase):
     def setUp(self):
@@ -10,6 +15,11 @@ class Test1(unittest.TestCase):
         self.result2 = average_run_time_excluding_system("test2.txt")
         self.result3 = count_app_failures3("test2.txt")
         self.result41, self.result42  = count_app_failures("test2.txt")
+        self.result51, self.result52  = app_with_most_successful_runs("test2.txt")
+        self.result61, self.result62 = third_of_day_with_most_failures("test2.txt")
+        self.result7= run_times_per_app("test2.txt")
+        self.result8= busiest_hour_per_app("test2.txt")
+        self.result9= calculate_failure_rates("test2.txt")
 
     def test_(self):
         self.assertEqual(self.result[('ERROR', 'BackendApp')],1)
@@ -24,16 +34,41 @@ class Test1(unittest.TestCase):
         self.assertEqual(self.result2[('BackendApp')],17.25)
         self.assertEqual(self.result2[('API')],18.67)
 
-    def test3(self):
+    def test_3(self):
         self.assertEqual(self.result3.get('FrontendApp'), 0)
         self.assertEqual(self.result3.get('BackendApp'),1)
         self.assertEqual(self.result3.get('API'),1)
         self.assertEqual(self.result3.get('SYSTEM'),0)
 
-    def test4(self):
+    def test_4(self):
         self.assertEqual(self.result41,"BackendApp")
         self.assertEqual(self.result42,1)
 
+    def test_5(self):
+        self.assertEqual(self.result51,"BackendApp")
+        self.assertEqual(self.result52,4)
+    
+    def test_6(self):
+        self.assertEqual(self.result61,'00:00:00 - 07:59:59')
+        self.assertEqual(self.result62,1)
+    
+    def test_7(self):
+        self.assertEqual(self.result7[0].get('BackendApp'),('13:08:50', 20))
+        self.assertEqual(self.result7[0].get('API'), ('03:42:50', 22))
+        self.assertEqual(self.result7[1].get('BackendApp'),('02:51:24', 15))
+        self.assertEqual(self.result7[1].get('API'),('17:23:35', 14))
+    
+    def test_8(self):
+        self.assertEqual(self.result8.get('BackendApp'),('01', 2))
+        self.assertEqual(self.result8.get('API'), ('17', 4))
+        self.assertEqual(self.result8.get('FrontendApp'),('13', 1))
+    
+    def test_9(self):    
+        self.assertEqual(self.result9.get('BackendApp'), 11.11)
+        self.assertEqual(self.result9.get('API'), 12.50)
+        self.assertEqual(self.result9.get('SYSTEM'), 0.0)
+        self.assertEqual(self.result9.get('FrontendApp'), 0.0)
         
+
 if __name__ == '__main__':
     unittest.main()
